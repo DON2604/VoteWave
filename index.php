aea313dc9c6e689d5c14b6190bedbb01bebd8802
Mrinmay Das <mrinmaydas2604gmail.com>
2024-03-09 11:14:26 +0530

    initial framework

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voting system</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./frontend/css/index.css">
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./frontend//assets/votlog.png" alt="Bootstrap Logo" width="50" height="50" class="d-inline-block align-text-top">
+                <span class="align-middle" style="position:relative; top: 5px;"><strong>VoteWave</strong></span>
+            </a>
+            <div class="navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav custom-left"> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home &nbsp  &nbsp</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Program &nbsp  &nbsp</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Help &nbsp  &nbsp</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./frontend/pages/contact.html">Contact &nbsp  &nbsp</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav custom-right"> 
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-outline-primary" href="./frontend/pages/login.html">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-outline-primary" href="#">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="hero">
+        New Way To<br> Empower Your<br><div class="voice"> Voice</div>
+        <div class="extra">VoteWave: Empowering voices through innovative e-voting.<br> Secure, accessible, and efficient, ensuring democratic participation.</div>
+
+    <div class="heroimage">
+        <img src="./frontend/assets/voter.png" alt="Description of your image"></div>
+    <div class="sky-ball">
+    </div>
+</div></div>
+<button class="buttons">Get Started</button>
+</body>
+</html>
